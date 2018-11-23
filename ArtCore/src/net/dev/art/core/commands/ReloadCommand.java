61cf278c04dbf6efce97daac160d24560506125c
BuildTools <unconfigured@null.spigotmc.org>
2018-11-23 17:22:16 -0200

    a

--- ArtCore/src/net/dev/art/core/commands/ReloadCommand.java
+++ ArtCore/src/net/dev/art/core/commands/ReloadCommand.java
@@ -3,28 +3,29 @@ package net.dev.art.core.commands;
 import java.util.Arrays;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
 import net.dev.art.core.ArtCore;
+import net.dev.art.core.ArtLib;
+import net.dev.art.core.ArtPlugin;
 import net.dev.art.core.managers.CommandManager;
 
-public class ReloadCommand extends CommandManager {
+public class ReloadCommand implements CommandExecutor, ArtLib {
 
-	public ReloadCommand(Plugin pl) {
-		setPlugin(pl);
-		register();
-		setPermission("admin.reload");
-		setPermissionMessage(getPermissionMessage());
-		setName("artreload");
-		setAliases(Arrays.asList("artrl", "reloadart"));
+	public ReloadCommand(ArtPlugin pl) {
+		pl.setCommand("artreload", this);
+		pl.getCommand("artreload").setAliases(Arrays.asList("artrl", "reloadart", "rlart"));
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String lb, String[] args) {
-		mensagem(sender, "§aPlugin sendo recarregado.");
-		ArtCore.getInstance().reload();
-		mensagem(sender, "§aPlugin recarregado.");
+		if (sender.hasPermission("reload.art")) {
+			mensagem(sender, "§aPlugin sendo recarregado.");
+			ArtCore.getInstance().reload();
+			mensagem(sender, "§aPlugin recarregado.");
+		}
 		return false;
 	}
 
